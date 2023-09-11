8de9e33d532f1744f82180d8ce058adc209c3169
denis-kosobanov <Kosobanovden@mail.ru>
2023-09-11 14:48:14 +0500

    init

---
+++ app/datebase.py
@@ -0,0 +1,18 @@
+from typing import AsyncGenerator
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+
+from app.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from app.models.base_model import Base
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
---
+++ app/main.py
@@ -0,0 +1,6 @@
+import asyncio
+
+from app.datebase import async_main
+
+if __name__ == '__main__':
+    asyncio.run(async_main())
